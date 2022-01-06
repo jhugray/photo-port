d5a1b94b32264fdbfd1e114665cb4e4ca10c8171
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-06 12:43:59 -0500

    modal added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Gallery from "./components/Gallery";
-import ContactForm from "./components/Contact";
-
-const [contactSelected, setContactSelected] = useState(false);
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -19,6 +17,8 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
@@ -29,7 +29,6 @@ function App() {
         setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
         {!contactSelected ? (
           <>
             <Gallery currentCategory={currentCategory}></Gallery>
@@ -38,7 +37,6 @@ function App() {
         ) : (
           <ContactForm></ContactForm>
         )}
-        </div>
       </main>
     </div>
   );
